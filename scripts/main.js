f9b8edb9da52a4f6f84a7fa7b562981a832f1e6e
jhath16 <hathaway.justin@gmail.com>
2015-01-17 20:35:46 -0500

    makes header position fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,25 @@
 $(document).ready(function () {
 
+	var headerHeight = $('header').outerHeight();
+	
+	$('.offset').css('height',headerHeight);
+
 	$('header li').click(function (e) {
 		var targ = '.' + e.target.id;
 
 		$('body').animate({
-			scrollTop: $(targ).last().offset().top
+			scrollTop: $(targ).last().offset().top - headerHeight
 		},1500);
 	});
 
 	$('#contact-button').click(function(e){
 		e.preventDefault();
 		$('body').animate({
-			scrollTop: $('.contact').offset().top
+			scrollTop: $('.contact').offset().top - headerHeight
 		},1500)
 	})
 
 	setTimeout(function () {
-		console.log($('h1'));
 		$('h1').addClass('text-animation');
 		$('.background').addClass('background-animation');
 		$('.background').removeClass('initial-background');
